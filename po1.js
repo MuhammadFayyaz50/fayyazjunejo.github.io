a3174fe0ed020dfa4c546edeb0a8717220609164
Muhammad Fayyaz <muhammadfayyaz565@yahoo.com>
2021-01-19 20:28:12 +0500

    project 01

---
+++ po1.js
@@ -0,0 +1,19 @@
+
+const form = document.getElementById('form');
+const Username = document.getElementById('Username');
+const Email = document.getElementById('Email');
+const Password = document.getElementById('Password');
+const Password2 = document.getElementById('Password2');
+
+//All function
+
+
+
+
+//add listener when submit//
+form.addEventListener('submit',function(e) {
+	e.preventDefault();
+	if (Username.value === '') {
+
+	}
+})
